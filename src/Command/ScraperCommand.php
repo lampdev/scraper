4b4519077976b79374675b51b13b5eba6ee2d6de
LAMPDev <orders@lamp-dev.com>
2020-03-22 20:33:20 +0100

    * DI configuration; * adding monolog

--- src/Command/ScraperCommand.php
+++ src/Command/ScraperCommand.php
@@ -8,10 +8,26 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Monolog\Logger;
 
 class ScraperCommand extends Command {
-  // the name of the command (the part after "bin/console")
-  protected static $defaultName = 'app:run-scraper';
+  /**
+   * @var ParameterBagInterface
+   */
+  private ParameterBagInterface $parameters;
+
+  /**
+   * @var Logger
+   */
+  private $logger;
+
+  public function __construct(ParameterBagInterface $parameters, Logger $logger, string $name = null) {
+    parent::__construct($name);
+
+    $this->parameters = $parameters;
+    $this->logger = $logger;
+  }
 
   protected function configure() {
     $this
@@ -20,7 +36,7 @@ class ScraperCommand extends Command {
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $output->write('Hello');
+    $output->write('Hello ' . $this->parameters->get('base_url'));
 
     return 0;
   }
